ca6d57fc10a4c4865ed2464d710410e93389fd9f
Edwin-Farid <117edwin@gmail.com>
2023-12-05 20:08:53 +0800

    feat: add content main activity

--- app/src/main/java/com/dicoding/e_doc/MainActivity.kt
+++ app/src/main/java/com/dicoding/e_doc/MainActivity.kt
@@ -3,14 +3,24 @@ package com.dicoding.e_doc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dicoding.e_doc.ui.theme.EdocTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,14 +42,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
+    Column(modifier = Modifier.padding(8.dp), horizontalAlignment = Alignment.Start) {
+        Text(text = "Sign Up")
+        Text(text = "Selamat Datang")
+        Image(
+            painter = painterResource(R.drawable.selfie),
+            contentDescription = "Selife image",
+            modifier = Modifier.size(80.dp),
         )
-        Text(text = "Edwin Farid")
-        Text(text = "Farid")
+        Text(text = "Email")
+        Text(text = "Password")
+        Text(text = "Konfirmasi Password")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Daftar")
+        }
+        Text(text = "Sudah Punya Akun?")
+
     }
+
 
 }
 
